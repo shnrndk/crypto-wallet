37e2ade2ea25211e16ab491876f948fe7092a6e0
Sahan <shnrndk@gmail.com>
2021-03-05 17:05:18 +0530

    home view

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypt_wallet/ui/add_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -10,8 +13,48 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Users')
+                  .doc(FirebaseAuth.instance.currentUser.uid)
+                  .collection('Coins')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return new ListView(
+                  children: snapshot.data.docs.map((DocumentSnapshot document) {
+                    return Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Coin Name:${document.id}'),
+                          Text('Amount Owned: ${document.data()['Amount ']}')
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              }),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Navigator.push(
+          //    context, MaterialPageRoute(builder: (context) => AddView()));
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
     );
   }
 }
